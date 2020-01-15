4a3c2cba0419e9fa266192f4705ab61e0980cbbd
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2020-01-15 16:27:08 +0300

    'calculator & decorator'

---
+++ core/calculator.py
@@ -0,0 +1,25 @@
+from hhhomework.core.decorators import cache_decorator
+
+
+@cache_decorator
+def calculator(a: float, b: float, operation: str) -> float:
+    # Здесь нужно реализовать функцию,
+    # которая реализует основные арифметические операции между числами: +, -, /, *, **.
+    # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
+    math_funk = {'+': lambda a, b: a + b,
+           '-': lambda a, b: a - b,
+           '/': lambda a, b: a / b,
+           '*': lambda a, b: a * b,
+           '**': lambda a, b: a ** b}
+    op = math_funk.get(operation, lambda a, b: 'Вы ввели не подерживаемую операцию')
+    return op(a, b)
+
+
+if __name__ == '__main__':
+    try:
+        a = float(input('Введите число: '))  # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
+        b = float(input('Введите число: '))
+        operation = input('Введите операцию: ')
+        print('Результат: ', calculator(a, b, operation))
+    except ValueError as err:
+        print(err, 'Вы ввели не число')
