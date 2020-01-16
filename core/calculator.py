d2fd57232f2258fcfcb3785db1d269d99e8e411e
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2020-01-16 15:05:42 +0300

    'all'

--- core/calculator.py
+++ core/calculator.py
@@ -7,17 +7,18 @@ def calculator(a: float, b: float, operation: str) -> float:
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
     math_funk = {'+': lambda a, b: a + b,
-           '-': lambda a, b: a - b,
-           '/': lambda a, b: a / b,
-           '*': lambda a, b: a * b,
-           '**': lambda a, b: a ** b}
+                 '-': lambda a, b: a - b,
+                 '/': lambda a, b: a / b,
+                 '*': lambda a, b: a * b,
+                 '**': lambda a, b: a ** b}
     op = math_funk.get(operation, lambda a, b: 'Вы ввели не подерживаемую операцию')
     return op(a, b)
 
 
 if __name__ == '__main__':
+    # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
     try:
-        a = float(input('Введите число: '))  # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
+        a = float(input('Введите число: '))
         b = float(input('Введите число: '))
         operation = input('Введите операцию: ')
         print('Результат: ', calculator(a, b, operation))
